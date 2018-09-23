d633b136288932188c46200a136d7af25df06bd7
aa-software2112 <a_dreoli@encs.concordia.ca>
2018-09-23 15:14:19 -0400

    Added initial header/source files for development

---
+++ RiskApplication/src/main.cpp
@@ -0,0 +1,20 @@
+/*
+ * main.cpp
+ *
+ *  Created on: Sep. 23, 2018
+ *      Author: Anthony Andreoli
+ */
+#include <iostream>
+
+
+int main()
+{
+
+	std::cout << "IN MAIN" << std::endl;
+
+
+	return 0;
+}
+
+
+
---
+++ RiskApplication/src/template.cpp
@@ -0,0 +1,4 @@
+
+#define TEMPLATE_LOCAL
+#include "template.h"
+
